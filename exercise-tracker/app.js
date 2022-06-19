de04d706a276e3095b6fb244e86621b11003ce18
Lart <tlartigau.pro@gmail.com>
2022-06-19 21:20:44 +0200

    small fix to pass fcc tests

--- exercise-tracker/app.js
+++ exercise-tracker/app.js
@@ -77,7 +77,7 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
   res.json({
     username: user.username,
     description: description,
-    duration: duration,
+    duration: Number(duration),
     date: date,
   })
 })
